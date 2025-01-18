25711f64ebd05dc628435d5ef7558d76c6c66bc0
bukanprogrammr <dzikriansah_s1sisfo2019@mahasiswa.ung.ac.id>
2025-01-18 18:33:24 +0700

    slice footer outer

--- resources/views/layouts/outer/footer.blade.php
+++ resources/views/layouts/outer/footer.blade.php
@@ -1,55 +1,91 @@
-<footer class="text-white bg-[#001C0B] mt-10 relative z-10">
-    <div class="container mx-auto p-6 grid grid-cols-1 md:grid-cols-3 gap-4 py-24 md:px-40">
-        <!-- Kontak Kami Section -->
-        <div class="mb-8">
-            <h3 class="font-bold text-lg mb-4 title-with-border">Kontak Kami</h3>
-            <p class="mb-2">Jl. Moh Van Gobel No. 270</p>
-            <p><a href="mailto:bsip.gorontalo@pertanian.go.id" class="underline">bsip.gorontalo@pertanian.go.id</a></p>
-        </div>
-        
-        <div class="mb-8">
-            <h3 class="font-bold text-lg mb-4 title-with-border">Statistik Web</h3>
-            <p class="mb-2">Total Pengunjung: <span class="font-bold"> $totalVisitors </span></p>
-            <p class="mb-2">Hari ini: <span class="font-bold"> $todayVisitors </span></p>
-            <p>Kemarin: <span class="font-bold"> $yesterdayVisitors </span></p>
-        </div>
-        
-        
-    
-        <!-- Map Section -->
-        <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d249.3520490752413!2d123.11686579872014!3d0.5525554164294187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x327ed5a6d6802eed%3A0x56bdf0afcf4fd9b0!2sBadan%20Standardisasi%20Intrumen%20Pertanian%20(BSIP)%20Gorontalo!5e0!3m2!1sid!2sid!4v1723743899093!5m2!1sid!2sid" height="203" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-2xl md:w-96 "></iframe>
-        </div>
-    </div>
-    
+<footer class="flex flex-col items-center bg-primary text-center text-surface text-slate pt-9">
+    <img src="/asset/brand.svg" alt="" class="h-auto w-96"/>
+  <div class="container pt-9">
+    <div class="flex flex-wrap justify-center items-center">
+     
+      <div class="flex flex-col items-center mb-6 space-y-4 px-20">
+        <p class="text-xl font-semibold text-surface text-slate">
+          Ikuti Kami :
+        </p>
 
-    <div class="border-t border-grey p-3">
-        <div class="container mx-auto p-4 flex justify-between items-center md:px-40">
-            <!-- Copyright Section -->
-            <p class="text-sm">
-                Copyrights © 2024 - <span class="font-bold">Badan Standardisasi Instrumen Pertanian, <br>Kementerian Pertanian Gorontalo</span>, all Rights Reserved.
-            </p>
-            <!-- Social Media Links -->
-            <div class="flex space-x-4">
-                <a href="#" class="hover:underline">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-5 h-5 rounded-md">
-                        <path d="M22.675 0h-21.35C.597 0 0 .597 0 1.326v21.348C0 23.403.597 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.892-4.788 4.655-4.788 1.325 0 2.463.099 2.795.143v3.243h-1.918c-1.503 0-1.794.714-1.794 1.763v2.31h3.587l-.467 3.622h-3.12V24h6.117c.728 0 1.325-.597 1.325-1.326V1.326C24 .597 23.403 0 22.675 0z"/>
-                    </svg>
-                    
-                </a>
-                <a href="#" class="hover:underline">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-5 h-5 rounded-md">
-                        <path d="M23.954 4.569c-.885.392-1.83.656-2.825.775 1.014-.608 1.793-1.574 2.163-2.723-.951.566-2.005.978-3.127 1.2a4.512 4.512 0 00-7.691 4.116c-3.746-.188-7.066-1.979-9.293-4.707a4.511 4.511 0 001.399 6.04 4.481 4.481 0 01-2.044-.567v.056a4.518 4.518 0 003.617 4.425c-.5.137-1.028.211-1.571.211-.382 0-.754-.038-1.112-.106a4.516 4.516 0 004.215 3.126 9.05 9.05 0 01-6.69 1.876 12.822 12.822 0 006.92 2.036c8.307 0 12.847-6.874 12.847-12.847 0-.196-.004-.392-.012-.587a9.192 9.192 0 002.25-2.338z"/>
-                    </svg>
-                    
-                </a>
-                <a href="#" class="hover:underline">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="w-5 h-5 rounded-md">
-                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.17.056 1.96.244 2.416.418a4.925 4.925 0 011.69 1.09 4.925 4.925 0 011.09 1.69c.174.456.362 1.246.418 2.416.058 1.267.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.056 1.17-.244 1.96-.418 2.416a4.92 4.92 0 01-1.09 1.69 4.926 4.926 0 01-1.69 1.09c-.456.174-1.246.362-2.416.418-1.267.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.17-.056-1.96-.244-2.416-.418a4.922 4.922 0 01-1.69-1.09 4.922 4.922 0 01-1.09-1.69c-.174-.456-.362-1.246-.418-2.416-.058-1.267-.07-1.646-.07-4.85s.012-3.584.07-4.85c.056-1.17.244-1.96.418-2.416a4.922 4.922 0 011.09-1.69 4.922 4.922 0 011.69-1.09c.456-.174 1.246-.362 2.416-.418C8.416 2.175 8.796 2.163 12 2.163zm0-2.163C8.735 0 8.333.013 7.052.073 5.772.132 4.752.324 3.91.632a7.084 7.084 0 00-2.49 1.645A7.09 7.09 0 00.632 3.91c-.308.842-.5 1.862-.559 3.142C.013 8.333 0 8.735 0 12s.013 3.667.073 4.948c.059 1.28.251 2.3.559 3.142a7.093 7.093 0 001.645 2.49 7.084 7.084 0 002.49 1.645c.842.308 1.862.5 3.142.559C8.333 23.987 8.735 24 12 24s3.667-.013 4.948-.073c1.28-.059 2.3-.251 3.142-.559a7.084 7.084 0 002.49-1.645 7.084 7.084 0 001.645-2.49c.308-.842.5-1.862.559-3.142.06-1.281.073-1.683.073-4.948s-.013-3.667-.073-4.948c-.059-1.28-.251-2.3-.559-3.142a7.084 7.084 0 00-1.645-2.49 7.084 7.084 0 00-2.49-1.645C19.333.132 18.313.13 17.032.073 15.751.013 15.349 0 12 0zm0 5.838a6.162 6.162 0 00-6.162 6.162A6.162 6.162 0 0012 18.162 6.162 6.162 0 0018.162 12 6.162 6.162 0 0012 5.838zm0 10.123a3.961 3.961 0 110-7.923 3.961 3.961 0 010 7.923zm6.406-11.845a1.44 1.44 0 11-2.88 0 1.44 1.44 0 012.88 0z"/>
-                    </svg>
-                                     
-                </a>
-            </div>
+        <div class="flex justify-center space-x-2">
+          <a
+            href="#!"
+            type="button"
+            class="rounded-full bg-transparent p-3 font-medium uppercase leading-normal text-surface transition duration-150 ease-in-out hover:bg-secondary focus:outline-none focus:ring-0 text-slate hover:bg-secondary-900"
+            data-twe-ripple-init>
+            <span class="[&>svg]:h-5 [&>svg]:w-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#eee"
+                viewBox="0 0 320 512">
+                <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+              </svg>
+            </span>
+          </a>
+
+          <a
+            href="#!"
+            type="button"
+            class="rounded-full bg-transparent p-3 font-medium uppercase leading-normal text-surface transition duration-150 ease-in-out hover:bg-secondary focus:outline-none focus:ring-0 text-slate hover:bg-secondary-900"
+            data-twe-ripple-init>
+            <span class="[&>svg]:h-5 [&>svg]:w-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#eee"
+                viewBox="0 0 512 512"
+                >
+                <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+              </svg>
+            </span>
+          </a>
+
+      <a
+        href="#!"
+        type="button"
+        class="rounded-full bg-transparent p-3 font-medium uppercase leading-normal text-surface transition duration-150 ease-in-out hover:bg-secondary focus:outline-none focus:ring-0 text-slate hover:bg-secondary-900"
+        data-twe-ripple-init>
+        <span class="mx-auto [&>svg]:h-5 [&>svg]:w-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#eee"
+            viewBox="0 0 448 512">
+            <path
+              d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+          </svg>
+        </span>
+      </a>
+
+      <a
+        href="#!"
+        type="button"
+        class="rounded-full bg-transparent p-3 font-medium uppercase leading-normal text-surface transition duration-150 ease-in-out hover:bg-secondary focus:outline-none focus:ring-0 text-slate hover:bg-secondary-900"
+        data-twe-ripple-init>
+        <span class="mx-auto [&>svg]:h-5 [&>svg]:w-5">
+        <svg fill="#eee" width="800px" height="800px" viewBox="0 0 512 512" id="icons" xmlns="http://www.w3.org/2000/svg"><path d="M412.19,118.66a109.27,109.27,0,0,1-9.45-5.5,132.87,132.87,0,0,1-24.27-20.62c-18.1-20.71-24.86-41.72-27.35-56.43h.1C349.14,23.9,350,16,350.13,16H267.69V334.78c0,4.28,0,8.51-.18,12.69,0,.52-.05,1-.08,1.56,0,.23,0,.47-.05.71,0,.06,0,.12,0,.18a70,70,0,0,1-35.22,55.56,68.8,68.8,0,0,1-34.11,9c-38.41,0-69.54-31.32-69.54-70s31.13-70,69.54-70a68.9,68.9,0,0,1,21.41,3.39l.1-83.94a153.14,153.14,0,0,0-118,34.52,161.79,161.79,0,0,0-35.3,43.53c-3.48,6-16.61,30.11-18.2,69.24-1,22.21,5.67,45.22,8.85,54.73v.2c2,5.6,9.75,24.71,22.38,40.82A167.53,167.53,0,0,0,115,470.66v-.2l.2.2C155.11,497.78,199.36,496,199.36,496c7.66-.31,33.32,0,62.46-13.81,32.32-15.31,50.72-38.12,50.72-38.12a158.46,158.46,0,0,0,27.64-45.93c7.46-19.61,9.95-43.13,9.95-52.53V176.49c1,.6,14.32,9.41,14.32,9.41s19.19,12.3,49.13,20.31c21.48,5.7,50.42,6.9,50.42,6.9V131.27C453.86,132.37,433.27,129.17,412.19,118.66Z"/></svg>
+        </span>
+      </a>
+
         </div>
+        <p class="text-lg text-surface text-slate">
+        {{ $appset->address }}
+        </p>
+      </div>
+
+      <div class="mb-6 w-full md:w-auto px-20 items-center flex justify-center">
+        <iframe
+          title="Google Maps"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.5973740742725!2d123.07952357581861!3d0.6029848635194317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32792b5377da427b%3A0xeee2a3b90b3ff61f!2sNokturnal_sp!5e0!3m2!1sid!2sid!4v1730261089336!5m2!1sid!2sid"
+          class="rounded-md shadow-lg desktop:w-[35rem] h-64 hp:w-72"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
+  </div>
+
+  <div class="w-full bg-black/5 p-4 text-center">
+    © 2023 Copyright: <a href="https://tw-elements.com/">TW Elements</a>
+  </div>
 </footer>
