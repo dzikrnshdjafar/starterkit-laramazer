d8885ffd8a914189ed34059d385bcf718da1db28
bukanprogrammr <dzikriansah_s1sisfo2019@mahasiswa.ung.ac.id>
2025-01-18 18:35:01 +0700

    slice footer outer

--- resources/views/pages/outer/index.blade.php
+++ resources/views/pages/outer/index.blade.php
@@ -94,6 +94,7 @@
     
       <!-- Footer -->
       @include("layouts.outer.footer")
+      
     <a href="https://wa.me/{{ $appset->phone_number }}" target="_blank" class="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg flex items-center space-x-2">
         <img src="{{ asset('landpage') }}/assets/logo/whatsapp.svg" alt="WhatsApp" class="w-full h-full">
       </a>
